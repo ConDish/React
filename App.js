937468796b2b5f7dafdf1c6915897b013e39e592
ConDish <samioff15@gmail.com>
2018-09-24 21:48:17 -0500

    Se hicieron cambios

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import {
-   StyleSheet,
-   Text,
-    View,
-    Button,
-    TouchableOpacity,
-    TextInput
-   } from 'react-native';
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TouchableOpacity,
+  TextInput
+} from 'react-native';
 import { createStackNavigator } from 'react-navigation';
 import md5 from 'md5';
-import Indexx from './app/index.js';
+import Logeado from './app/index.js';
+import Toast, { DURATION } from 'react-native-easy-toast';
 
 
 export default class App extends React.Component {
@@ -21,17 +22,17 @@ export default class App extends React.Component {
 
 class Primera extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     super(props)
 
     this.state = {
-        usuario : '',
-        contra : '',
+      usuario: '',
+      contra: '',
     }
   }
   static navigationOptions = {
 
-    title: 'Primera',
+    title: 'Inicio',
 
   };
 
@@ -41,81 +42,77 @@ class Primera extends React.Component {
   }
 
   changeContra = (contra) => {
-    this.setState({contra : contra})
+    this.setState({ contra: contra })
   }
 
-  envioData(){
+  envioData() {
 
-      const {navigate} = this.props.navigation;
+    const { navigate } = this.props.navigation;
 
-      const Arraydata = [];
+    const Arraydata = [];
 
-      const data = {
+    const data = {
 
-        nombre : this.state.usuario,
-        contra : md5(this.state.contra)
+      nombre: this.state.usuario,
+      contra: md5(this.state.contra)
 
-      }
-      Arraydata.push(data);
-      
-      
-      
-      console.log(Arraydata[0].contra);
+    }
+    Arraydata.push(data);
 
-      navigate('envio', {nombre : Arraydata[0].nombre});
+    // Validar el usuario y la contraseña
+    if (Arraydata[0].nombre == "admin" && Arraydata[0].contra == md5('123456')) {
+
+      navigate('ir', { nombre: Arraydata[0].nombre });
+
+    } else {
+      // Borrar todo lo que haya escrito en los inputs de logeo
+      this.changeUsuario('');
+      this.changeContra('');
+
+      // Mostrar el toast por datos incorrectos
+      this.refs.toast.show(<Text style={{ textAlign: "center", color: "#FFF"}}>Datos Incorrectos!</Text>);
+    }
+
   }
 
-  
+
   render() {
 
-    
-    
+
+
     return (
       <View style={styles.container}>
 
         <TextInput
-        style={styles.inputTop}
-        placeholder="Usuario"
-        value={this.state.usuario}
-        onChangeText={this.changeUsuario}
+          style={styles.inputTop}
+          placeholder="Usuario"
+          value={this.state.usuario}
+          onChangeText={this.changeUsuario}
         />
 
         <TextInput
-        style={styles.input}
-        placeholder="Contraseña"
-        value={this.state.contra}
-        onChangeText={this.changeContra}
+          style={styles.input}
+          placeholder="Contraseña"
+          value={this.state.contra}
+          onChangeText={this.changeContra}
         />
 
-        
+
         <TouchableOpacity
           style={styles.boton}
           onPress={() => this.envioData()}
           underlayColor='#fff'>
-            <Text style={styles.textboton}>Enviar</Text>
+          <Text style={styles.textboton}>Enviar</Text>
         </TouchableOpacity>
+        
+          <Toast ref="toast" style={styles.toast}/>
+        
       </View>
     );
   }
 }
 
-class Prueba extends React.Component{
-  static navigationOptions = {
 
-    title: 'Segunda',
-
-  };
-  
-  render(){
-    const {navigation} = this.props;
-    const getnombre = navigation.getParam('nombre');
-    return(
-      <View style={styles.container}>
-        <Text>{JSON.stringify(getnombre)}</Text>
-      </View>
-    )
-  }
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -123,12 +120,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     paddingLeft: 15,
     paddingRight: 15
-   
+
   },
 
   inputTop: {
 
-    height : 40,
+    height: 40,
     borderWidth: 0,
     marginBottom: 20,
     marginTop: 100,
@@ -139,7 +136,7 @@ const styles = StyleSheet.create({
 
   input: {
 
-    height : 40,
+    height: 40,
     marginTop: 20,
     marginBottom: 40,
     paddingLeft: 5,
@@ -149,7 +146,7 @@ const styles = StyleSheet.create({
 
   boton: {
     height: 40,
-    backgroundColor:'#1E6738',
+    backgroundColor: '#1E6738',
     borderRadius: 10,
     borderWidth: 1,
     borderColor: '#fff',
@@ -157,20 +154,25 @@ const styles = StyleSheet.create({
     paddingBottom: 10,
   },
 
-  textboton : {
+  textboton: {
     color: "#fff",
     textAlign: 'center',
     paddingLeft: 10,
     paddingRight: 10,
-    fontSize : 18
-  }
+    fontSize: 18
+  },
+  toast: {
+    backgroundColor: '#FFC300',
+    width: '100%',
+    
+  },
 
 });
 
 const MyNavigator = createStackNavigator(
-    {
-      send: Primera,
-      envio: Prueba
-    }
+  {
+    send: Primera,
+    ir: Logeado
+  }
 
 )
--- app/index.js
+++ app/index.js
@@ -1,40 +1,159 @@
 import React from 'react';
 import {
-   StyleSheet,
-   Text,
-    View,
-    Button,
-    TouchableOpacity,
-    TextInput
-   } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
+  StyleSheet,
+  Image,
+  View,
+  FlatList
+} from 'react-native';
+import { createMaterialTopTabNavigator, createStackNavigator } from 'react-navigation';
+import { Container, Header, Title, Left, Right, Button, Body, Content, Text, Card, CardItem } from 'native-base';
 
 
-class Prueba extends React.Component{
+// Cuando la persona se logea
+class Logeado extends React.Component {
   static navigationOptions = {
 
-    title: 'Segunda',
+    title: 'Inicio',
+    tabBarIcon: ({ tintColor }) => (
+      <Image source={require('./img/home.png')}
+
+      />
+    ),
+
 
   };
-  
-  render(){
-    const {navigation} = this.props;
+
+  render() {
+
+    const { navigation } = this.props;
     const getnombre = navigation.getParam('nombre');
-    return(
-      <View style={styles.container}>
-        <Text>{JSON.stringify(getnombre)}</Text>
-      </View>
+    return (
+      <Container>
+        <Text>{getnombre}</Text>
+
+      </Container>
     )
+
   }
+
+
 }
 
 
+// Los productos
+class Productos extends React.Component {
+  static navigationOptions = {
+
+    title: 'Productos',
+    tabBarIcon: ({ tintColor }) => (
+      <Image source={require('./img/producto.png')} />
+    ),
+
+
+  };
+
+  constructor() {
+    super()
+    this.state = {
+      dataSource: []
+    }
+  }
+  renderItem = ({ item }) => {
+
+    return (
+      <View style={{ flex: 1, flexDirection: 'row', marginBottom: 3 }}>
+        <Image source={{ uri: item.picture }}
+          style={{ width: 80, height: 80, margin: 5 }}
+        />
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <Text style={{ fontSize: 18, color: 'green', marginBottom: 15}}>
+            {item.name}
+          </Text>
+        </View>
+
+      </View>
+    )
+
+  }
+
+
+
+  componentDidMount() {
+
+    const url = 'http://www.json-generator.com/api/json/get/cfRTKNAQya?indent=1';
+
+    fetch(url).then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          dataSource: responseJson
+        })
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={this.renderItem}
+          keyExtractor={(item, index) => index.toString()}
+
+        />
+      </View>
+    );
+  }
+
+
+
+}
+
+// Los CSS
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     paddingLeft: 15,
     paddingRight: 15
-   
+
+  },
+  colorIcon: {
+    paddingRight: 10,
+    color: '#fff',
   }
 })
+// El Tab Navigator
+export default createMaterialTopTabNavigator(
+  {
+    Home: { screen: Logeado },
+    Productos: { screen: Productos }
+  },
+  {
+    // Agregar estilos o iconos a mi tabnavigator
+
+
+    animationEnabled: true,
+    swipeEnabled: true,
+    tabBarPosition: 'bottom',
+    tabBarOptions: {
+      showIcon: true,
+      activeTintColor: '#FFF',
+      labelStyle: {
+        fontSize: 9,
+
+      },
+      style: {
+        backgroundColor: 'grey',
+      },
+      indicatorStyle: {
+        borderBottomColor: '#ffffff',
+        borderBottomWidth: 2,
+      },
+
+    },
+
+
+
+  });
